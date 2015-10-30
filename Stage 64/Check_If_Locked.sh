5aac2090c43807d51176170383d19792df0ff8b5
Logan Bown <loganpatrick99@gmail.com>
2015-10-30 00:52:37 -0500

    new file:   1 	new file:   2 	new file:   3 	new file:   4 	modified:   Authenticate.md 	modified:   Check_If_Locked.sh

--- Stage 64/Check_If_Locked.sh
+++ Stage 64/Check_If_Locked.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 while :
 do
-	s=$(TZ='America/New_York' date)
+	s=$(date +%s)
 	while read line
 	do
 		if [[ $(echo $line | sed 's/[0-9]\{1,2\}\. //') == 'LOCKED - US' ]]
 		then
 			s="$s -"
 		else
-			s="$s $line"
+			s="$s X"
+			echo "$line" > echo "$line" > "$(echo "$line"|sed 's/\..*//')"
 		fi
 	done < <(curl -u $(curl -s http://averyhardtest.com/421563647 | grep -Po "[0-9]{2,4}(?=:)"):$(curl -s ipinfo.io/ip) -s averyhardtest.com:64 | egrep \[0-9\]\{1,2\}\\.)
 	echo "$s" >> logfile
